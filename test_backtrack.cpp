5a13fec4055f8da40903e4731bbdaa10b8011fb7
spjung <spjung@humaxdigital.com>
2014-08-05 16:39:51 +0900

    [algospot] - confirm test_backtrack.cpp code - get generate_subsets code used backtrack

--- test_backtrack.cpp
+++ test_backtrack.cpp
@@ -2,15 +2,20 @@
 
 using namespace std;
 
-#define MAXARRAYLEN		3
+//#define USE_SUBSETS_FUNC
+#define NMAX			4
 #define MAXCANDIDATES	10
-bool finished = false;
+bool finished = false;		// 모든 풀이를 찾았는지 여부
 
+// 간단하게 말하자면 재귀에서 마지막 노드에 왔는가를 검사. 일종의 종료 조건.
 bool is_a_solution(int a[], int k, int n)
 {
-	return (k==n)?true:false;
+	return (k==n);//?true:false;
 }
 
+//결과의 순서에 가장 영향을 끼치는 부분. 이 부분을 수정하여 다른 문제로 응용이 가능
+//분기를 나누는데 결정되는 c배열의 값을 이 함수에서 구함.
+//아래 code는 부분집합을 구하는 경우라 후보군의 갯수가 있고(true), 없고(false) 두가지 뿐임
 void construct_candidates(int a[], int k, int n, int c[], int* ncandidates)
 {
 	c[0]=true;
@@ -18,6 +23,8 @@ void construct_candidates(int a[], int k, int n, int c[], int* ncandidates)
 	*ncandidates=2;
 }
 
+//마지막 노드까지 와서 출력하는 부분.(출력 이외의 output이 필요하면 이부분을 수정)
+//is_a_solution에서 k(현재 재귀호출된 count)와 입력한 n 값을 비교한 후 끝까지 오면 호출 됨
 void process_solution(int a[], int k)
 {
 	int i;
@@ -32,9 +39,9 @@ void process_solution(int a[], int k)
 
 void backtrack(int a[], int k, int input)
 {
-	int c[MAXCANDIDATES];
-	int ncandidates;
-	int i;
+	int c[MAXCANDIDATES] = {0,};	//다음 위치가 될 수 있는 후보군 배열, c[x]에는 후보군의 값이 들어 있음
+	int ncandidates;				//다음 위치가 될 수 있는 후보군 개수, construct_candidates에서 정해짐
+	int i;							//카운터
 	
 	if(is_a_solution(a,k,input))
 		process_solution(a,k);
@@ -47,15 +54,25 @@ void backtrack(int a[], int k, int input)
 		{
 			a[k]=c[i];
 			backtrack(a,k,input);
-			if(finished)
+			if(finished)		//일찍 종료할 조건이 있다면 backtrack 호출되기전에 값을 바꾸어서 응용 가능
 				return;
 		}
 	}
 }
 
+void generate_subsets(int n)
+{
+	int a[NMAX];
+	backtrack(a,0,n);
+}
+
+
 int main(void)
 {
-	int a[MAXARRAYLEN] = {1,2,3};
+#if defined (USE_SUBSETS_FUNC)
+	generate_subsets(3);
+#else
+	int a[NMAX] = {0,};
 	int k;
 	int n;	/* need resolved size */
 	
@@ -63,6 +80,7 @@ int main(void)
 	n = 3;
 	
 	backtrack(a, k, n);
+#endif
 
 	return 0;
 }
